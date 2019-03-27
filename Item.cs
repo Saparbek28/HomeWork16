ba6865b9ec6256bedf8257db44c9cc00e60cdbf8
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-03-28 03:35:44 +0600

    adding file

---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Item
+    {
+        public string Title { get; set; }
+        public string Guid { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2();
+        }
+
+        private static void Task2()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+
+            var students = xmlDocument.CreateElement("students");
+            xmlDocument.AppendChild(students);
+
+            var student = xmlDocument.CreateElement("student");
+            student.SetAttribute("FirstName", "Saparbek");
+            student.SetAttribute("LastName", "Bainazar");
+            students.AppendChild(student);
+
+            Console.WriteLine(xmlDocument.DocumentElement.FirstChild.Attributes["FirstName"].Value + " " + xmlDocument.DocumentElement.FirstChild.Attributes["LastName"].Value);
+            Console.ReadLine();
+        }
+
+        private static void Task1()
+        {
+            List<Item> list = new List<Item>();
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("habrahabr.ru.xml");
+            var items = xmlDocument.DocumentElement.GetElementsByTagName("item");
+            foreach (XmlElement item in items)
+            {
+                list.Add(new Item()
+                {
+                    Title = item.ChildNodes.Item(0).InnerText,
+                    Guid = item.ChildNodes.Item(1).InnerText,
+                    Link = item.ChildNodes.Item(2).InnerText,
+                    Description = item.ChildNodes.Item(3).InnerText,
+                    PubDate = item.ChildNodes.Item(4).InnerText,
+                });
+            }
+            foreach (var item in list)
+            {
+                Console.Write("{0}\n{1}\n{2}\n{3}\n{4}\n\n", item.Title, item.Guid, item.Link, item.Description, item.PubDate);
+            }
+            Console.ReadLine();
+        }
+    }
+}
